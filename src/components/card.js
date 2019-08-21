f28b301ebae78102492943e658c2b0e271783cae
Boris Davydenko <brsdv@yandex.ru>
2019-08-21 22:25:02 +0300

    module3-task1 completed

--- src/components/card.js
+++ src/components/card.js
@@ -1,30 +1,30 @@
-export const getCardTemplate = () => `<li class="trip-events__item">
+export const getCardTemplate = ({types, town, time, price, offers}) => `<li class="trip-events__item">
 <div class="event">
   <div class="event__type">
-    <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+    <img class="event__type-icon" width="42" height="42" src="${types.map((element) => element.img)}" alt="Event type icon">
   </div>
-  <h3 class="event__title">Taxi to airport</h3>
+  <h3 class="event__title">${types.map((element) => element.title)} ${town}</h3>
 
   <div class="event__schedule">
     <p class="event__time">
-      <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+      <time class="event__start-time" datetime="${new Date(time.date).toLocaleString(`en`, {day: `numeric`, month: `numeric`, year: `numeric`})}">${time.hour}:${time.minute}</time>
       &mdash;
-      <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+      <time class="event__end-time" datetime="${new Date(time.date).toLocaleString(`en`, {day: `numeric`, month: `numeric`, year: `numeric`})}">${time.hour + 1}:${time.minute}</time>
     </p>
-    <p class="event__duration">1H 30M</p>
+    <p class="event__duration">${(time.hour + 1) - time.hour}H</p>
   </div>
 
   <p class="event__price">
-    &euro;&nbsp;<span class="event__price-value">20</span>
+    &euro;&nbsp;<span class="event__price-value">${price}</span>
   </p>
 
   <h4 class="visually-hidden">Offers:</h4>
   <ul class="event__selected-offers">
-    <li class="event__offer">
-      <span class="event__offer-title">Order Uber</span>
-      &plus;
-      &euro;&nbsp;<span class="event__offer-price">20</span>
-     </li>
+    ${offers.map((element) => (offers.length > 0) ? `<li class="event__offer">
+    <span class="event__offer-title">${element.title}</span>
+    &plus;
+    &euro;&nbsp;<span class="event__offer-price">${element.price}</span>
+   </li>` : ``).join(``)}
   </ul>
 
   <button class="event__rollup-btn" type="button">
---
+++ src/data.js
@@ -0,0 +1,143 @@
+const TOTAL_CARD_COUNT = 4;
+
+const shuffle = function (array) {
+  let cloneArray = Array.from(array).slice();
+  let j;
+  let temp;
+
+  for (let i = cloneArray.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = cloneArray[j];
+    cloneArray[j] = cloneArray[i];
+    cloneArray[i] = temp;
+  }
+
+  return cloneArray;
+};
+
+const getRandomText = (number) => {
+  const discriptions = [
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    `Cras aliquet varius magna, non porta ligula feugiat eget.`,
+    `Fusce tristique felis at fermentum pharetra.`,
+    `Aliquam id orci ut lectus varius viverra.`,
+    `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
+    `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+    `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
+    `Sed sed nisi sed augue convallis suscipit in sed felis.`,
+    `Aliquam erat volutpat.`,
+    `Nunc fermentum tortor ac porta dapibus.`,
+    `In rutrum ac purus sit amet tempus.`
+  ];
+
+  return shuffle(discriptions).slice(0, number).join(` `);
+};
+
+const getDataEvents = () => ({
+  types: shuffle([
+    {
+      type: `bus`,
+      img: `img/icons/bus.png`,
+      title: `Taxi to`
+    },
+    {
+      type: `check`,
+      img: `img/icons/check-in.png`,
+      title: `Check into`
+    },
+    {
+      type: `drive`,
+      img: `img/icons/drive.png`,
+      title: `Drive to`
+    },
+    {
+      type: `flight`,
+      img: `img/icons/flight.png`,
+      title: `Flight to`
+    },
+    {
+      type: `restaurant`,
+      img: `img/icons/restaurant.png`,
+      title: `Restaurant to`
+    },
+    {
+      type: `ship`,
+      img: `img/icons/ship.png`,
+      title: `Ship to`
+    },
+    {
+      type: `sightseeing`,
+      img: `img/icons/sightseeing.png`,
+      title: `Sightseeing to`
+    },
+    {
+      type: `train`,
+      img: `img/icons/train.png`,
+      title: `Train to`
+    },
+    {
+      type: `transport`,
+      img: `img/icons/transport.png`,
+      title: `Transport to`
+    },
+    {
+      type: `trip`,
+      img: `img/icons/trip.png`,
+      title: `Trip to`
+    }
+  ]).slice(0, 1),
+  town: [
+    `Saint Petersburg`,
+    `Berlin`,
+    `Prague`,
+    `Lappeenranta`,
+    `Helsinki`,
+  ][Math.floor(Math.random() * 5)],
+  photos: new Array(3).fill(``).map(() => `http://picsum.photos/300/150?r=${Math.random()}`),
+  discription: getRandomText(Math.ceil(Math.random() * 3)),
+  time: {
+    date: Date.now() + 1 + Math.floor(Math.random() * 7) * 24 * 60 * 60 * 1000,
+    hour: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12][Math.floor(Math.random() * 13)],
+    minute: [10, 15, 20, 25, 30, 35, 40, 45, 50, 55][Math.floor(Math.random() * 10)]
+  },
+  price: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100][Math.floor(Math.random() * 10)],
+  offers: shuffle([
+    {
+      title: `Add luggage`,
+      price: 10,
+      isActive: Math.random() >= 0.5
+    },
+    {
+      title: `Switch to comfort`,
+      price: 150,
+      isActive: Math.random() >= 0.5
+    },
+    {
+      title: `Add meal`,
+      price: 2,
+      isActive: Math.random() >= 0.5
+    },
+    {
+      title: `Choose seats`,
+      price: 9,
+      isActive: Math.random() >= 0.5
+    },
+  ]).slice(0, Math.round(Math.random() * 2))
+});
+
+const getMenu = new Set([
+  `Table`,
+  `Stats`,
+]);
+
+const getFilter = new Set([
+  `Everything`,
+  `Future`,
+  `Past`,
+]);
+
+export const totalCards = new Array(TOTAL_CARD_COUNT).fill(``).map(getDataEvents);
+export const menuNames = Array.from(getMenu);
+export const filterNames = Array.from(getFilter);
+export const townsTrip = Array.from(new Set(totalCards.map((element) => element.town)));
+export const datesTrip = Array.from(new Set(totalCards.map((element) => element.time.date))).sort();
