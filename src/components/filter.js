d4e070b66c38cd7cc68bcfe2f638532302cda83b
Boris Davydenko <brsdv@yandex.ru>
2019-08-26 13:03:23 +0300

    module4-task1 completed

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,7 +1,21 @@
-export const getFilterTemplate = (filterNames) => `<form class="trip-filters" action="#" method="get">
-${filterNames.map((element) => `<div class="trip-filters__filter">
-<input id="filter-${element.toLowerCase()}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${element.toLowerCase()}" ${element === `Everything` ? `checked` : ``}>
-<label class="trip-filters__filter-label" for="filter-${element.toLowerCase()}">${element}</label>
-</div>`).join(``)}
-<button class="visually-hidden" type="submit">Accept filter</button>
-</form>`;
+import {createElement} from '../utils.js';
+
+export class Filter {
+  constructor(filterNames) {
+    this._filterNames = filterNames;
+  }
+
+  getTemplate() {
+    return `<form class="trip-filters" action="#" method="get">
+    ${ this._filterNames.map((element) => `<div class="trip-filters__filter">
+    <input id="filter-${element.toLowerCase()}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${element.toLowerCase()}" ${element === `Everything` ? `checked` : ``}>
+    <label class="trip-filters__filter-label" for="filter-${element.toLowerCase()}">${element}</label>
+    </div>`).join(``)}
+    <button class="visually-hidden" type="submit">Accept filter</button>
+    </form>`.trim();
+  }
+
+  getElement() {
+    return createElement(this.getTemplate());
+  }
+}
---
+++ src/utils.js
@@ -0,0 +1,31 @@
+const Position = {
+  AFTERBEGIN: `afterbegin`,
+  AFTEREND: `afterend`,
+  BEFOREEND: `beforeend`,
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+  return newElement.firstChild;
+};
+
+export const renderElement = (container, element, place = Position.BEFOREEND) => {
+  switch (place) {
+    case Position.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case Position.AFTEREND:
+      container.after(element);
+      break;
+    case Position.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
+export const remove = (element) => {
+  if (element) {
+    element.remove();
+  }
+};
